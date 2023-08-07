450e419c2dc5bc528c1d95dd846a94b6c07075e3
Suman20singh <suman.singh12681@gmail.com>
2023-08-08 00:05:36 +0530

    Proveway Task

---
+++ store.js
@@ -0,0 +1,31 @@
+const dataList = [
+  {
+    isBundleSelected: false,
+    pricePerPair: 195,
+    unit: "DKK",
+    discountPercent: 50,
+    pairData: [{ size: "S", color: "red" }],
+  },
+  {
+    isBundleSelected: false,
+    pricePerPair: 195,
+    unit: "DKK",
+    discountPercent: 40,
+    pairData: [
+      { size: "S", color: "red" },
+      { size: "S", color: "red" },
+    ],
+  },
+  {
+    isBundleSelected: false,
+    pricePerPair: 195,
+    unit: "DKK",
+    discountPercent: 60,
+    pairData: [
+      { size: "S", color: "red" },
+      { size: "S", color: "red" },
+      { size: "S", color: "red" },
+    ],
+  },
+];
+export { dataList };
